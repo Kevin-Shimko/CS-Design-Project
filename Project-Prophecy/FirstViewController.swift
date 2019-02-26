396c4c95ab40f651412902119850f532b9bfc698
Kevin-Shimko <43215259+Kevin-Shimko@users.noreply.github.com>
2019-02-26 18:38:48 -0500

    Initial Commit

---
+++ Project-Prophecy/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Project-Prophecy
+//
+//  Created by Kevin Shimko on 2/26/19.
+//  Copyright © 2019 KSS. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Project-Prophecy/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Project-Prophecy
+//
+//  Created by Kevin Shimko on 2/26/19.
+//  Copyright © 2019 KSS. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
