e4feee5e6549b71d6f0365a8601b79dc704d5bf2
Kevin-Shimko <43215259+Kevin-Shimko@users.noreply.github.com>
2019-03-27 14:45:39 -0400

    More updates

---
+++ Project-Prophecy/GameCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  GameCollectionViewCell.swift
+//  Project-Prophecy
+//
+//  Created by Kevin Shimko on 3/27/19.
+//  Copyright © 2019 KSS. All rights reserved.
+//
+
+import UIKit
+
+class GameCollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var game_title: UILabel!
+    @IBOutlet weak var game_releaseDate: UILabel!
+    @IBOutlet weak var game_poster: UIImageView!
+    
+}
---
+++ Project-Prophecy/TVCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TVCollectionViewCell.swift
+//  Project-Prophecy
+//
+//  Created by Kevin Shimko on 3/27/19.
+//  Copyright © 2019 KSS. All rights reserved.
+//
+
+import UIKit
+
+class TVCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var tv_title: UILabel!
+    @IBOutlet weak var tv_releaseDate: UILabel!
+    @IBOutlet weak var tv_poster: UIImageView!
+    
+    
+}
