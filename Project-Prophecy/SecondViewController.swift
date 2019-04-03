d13d40dfec1a4aae370218023b501076151cefa6
Kevin-Shimko <43215259+Kevin-Shimko@users.noreply.github.com>
2019-04-03 12:32:59 -0400

    UI updates

--- Project-Prophecy/SecondViewController.swift
+++ Project-Prophecy/SecondViewController.swift
@@ -9,10 +9,19 @@
 import UIKit
 
 class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var profile_image: UIImageView!
+    @IBOutlet weak var profile_username: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        profile_image.layer.borderWidth = 1
+        profile_image.layer.masksToBounds = false
+        profile_image.layer.borderColor = UIColor.black.cgColor
+        profile_image.layer.cornerRadius = profile_image.frame.height/2
+        profile_image.clipsToBounds = true
     }
 
 
